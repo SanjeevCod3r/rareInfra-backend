ade726c41e7c85c79b03b6c05b64c9e83a65a9c3
Anshika <anshikaverma4515@gmail.com>
2025-06-25 22:05:37 +0530

    api to fetch all contacts on form submision

--- controller/formcontroller.js
+++ controller/formcontroller.js
@@ -20,3 +20,26 @@ export const submitForm = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+
+export const getAllFormData = async (req, res) => {
+  try {
+    const forms = await Form.find().select('name email phone message');
+    res.json({success: true,forms});
+  } catch (error) {
+    console.error('Error fetching form data:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const deleteContact = async (req, res) => {
+  try {
+    const contact = await Form.findByIdAndDelete(req.body.id);
+    if (!contact) {
+      return res.status(404).json({ message: 'contact not found', success: false });
+    }
+    return res.json({ message: 'Contact deleted successfully', success: true });
+  } catch (error) {
+    console.error('Error deleting contact:', error);
+    return res.status(500).json({ message: 'Server Error', success: false });
+  }
+};
--- routes/formrouter.js
+++ routes/formrouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { submitForm } from '../controller/formcontroller.js';
+import { submitForm , getAllFormData , deleteContact } from '../controller/formcontroller.js';
 
 const router = express.Router();
 
 router.post('/submit', submitForm);
+router.get("/all", getAllFormData);
+router.post('/delete-contact', deleteContact);
 
 export default router;
