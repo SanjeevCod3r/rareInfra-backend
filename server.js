0c77d8f9e06852f52528de43384b846fe71e0786
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-07-31 10:37:44 +0530

    [Sanjeev] Add: Done with Route

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.use(cors({
     'https://buildestate.vercel.app',
     'https://real-estate-website-admin.onrender.com',
     'https://real-estate-website-backend-zfu7.onrender.com',
+    'https://rare-admin.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'], // Added HEAD
